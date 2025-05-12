f1f19680d11a58aba7fa4f8012125046b166c6d6
Script605 <s.raphael@escolar.ifrn.edu.br>
2025-05-12 12:03:23 -0300

    WOW

---
+++ Motores 1/Assets/script/player.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+
+
+public class payer : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        var numero = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         //Debug.Log(numero);
+         //numero = numero + 1; 
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.position += new Vector3(velocidade, 0, 0);
+        }
+
+        if (Input.)
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        lucario_move_left_and_right_using_the_keyboard: ;
+    }
+}

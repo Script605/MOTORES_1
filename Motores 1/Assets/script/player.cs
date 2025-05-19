d56ad1dd61bbfc8c15aebbfde2a80be24eb0e0cc
Script605 <s.raphael@escolar.ifrn.edu.br>
2025-05-19 11:55:49 -0300

    hamburgui

--- Motores 1/Assets/script/player.cs
+++ Motores 1/Assets/script/player.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class payer : MonoBehaviour
 {
+    private int numero;
+
+    public float velocidade = 20;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,35 +17,18 @@ public class payer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-         //Debug.Log(numero);
-         //numero = numero + 1; 
+        //Debug.Log(numero);
+        //numero = numero + 1; 
 
 
         if (Input.GetKey(KeyCode.A))
         {
-            gameObject.transform.position += new Vector3(velocidade, 0, 0);
+            gameObject.transform.position += new Vector3(-velocidade * Time.deltaTime, 0, 0);
         }
 
-        if (Input.)
-
-
-
-
-
-
-
-
-
-
-
-
-    }
-}
-
-internal class Program
-{
-    public static void Main(string[] args)
-    {
-        lucario_move_left_and_right_using_the_keyboard: ;
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.position += new Vector3(velocidade * Time.deltaTime, 0, 0);
+        }
     }
 }
